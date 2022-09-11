11f9e67f4be4722bf7ffa3632d7b2ae7c2f13bac
MLanfear <109485865+MLanfear@users.noreply.github.com>
2022-09-11 02:34:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ const printProfileData = (profileDataArr) => {
 };
 
 printProfileData(profileDataArgs);
+
+// Using function expression syntax
+const addNums = function(numOne, numTwo) {
+  return numOne + numTwo;
+};
+
+// Using new arrow function syntax
+const addNums = (numOne, numTwo) => {
+  return numOne + numTwo;
+};
