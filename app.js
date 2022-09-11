f4fc48a8109efe45cf132cded6cc688ca7951c9d
MLanfear <109485865+MLanfear@users.noreply.github.com>
2022-09-11 02:47:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 
-const printProfileData = (profileDataArr) => {
+const printProfileData = profileDataArr => {
   //This...
   for (let i = 0; i < profileDataArr.length; i+= 1) {
     console.log(profileDataArr[i]);
