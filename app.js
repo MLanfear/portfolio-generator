3ffe969beddcff8c537a4ec4a1dfe7da3d606a5e
MLanfear <109485865+MLanfear@users.noreply.github.com>
2022-09-12 03:19:22 -0700

    Update app.js

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ const promptProject = portfolioData => {
     
 
   ]);
-    .then(projectData => {
+    then(projectData => {
       portfolioData.projects.push(projectData);
       if (projectData.confirmAddProject) {
         return promptProject(portfolioData);
