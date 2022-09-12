f98fa2913f4c4fda4fae6f5f3341312013804d16
MLanfear <109485865+MLanfear@users.noreply.github.com>
2022-09-12 00:43:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const generatePage = (name, github) => {
 };
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error (err);
+  if (err) throw new Error(err);
   
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
