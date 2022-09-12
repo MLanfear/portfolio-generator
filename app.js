5f8bf810aa6ea89b12ef241a0ece6f5155e88d25
MLanfear <109485865+MLanfear@users.noreply.github.com>
2022-09-12 01:28:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ const inquirer = require('inquirer');
   
 //  console.log('Portfolio complete! Check out index.html to see the output!');
 //});
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
-    }
-  ])
-  .then(answers => console.log(answers));
+
+const promptUser = () => {
+  return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      }
+    ]);
+};
+promptUser().then(answers => console.log(answers));
