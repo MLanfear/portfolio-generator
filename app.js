dac24a3db7138d74299ff835700e406865a34511
MLanfear <109485865+MLanfear@users.noreply.github.com>
2022-09-12 04:33:04 -0700

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,14 @@ const promptProject = portfolioData => {
        =================
          `);     
   
-  return inquirer.prompt([
+  return inquirer.prompt([.then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
     {
       type: 'input',
       name: 'name',
@@ -83,15 +90,7 @@ const promptProject = portfolioData => {
       message: 'Would you like to enter another project?',
       default: false
     }
-      then(projectData => {
-        portfolioData.projects.push(projectData);
-          if (projectData.confirmAddProject) {
-            return promptProject(portfolioData);
-          } else {
-            return portfolioData;
-          }
-      })
-    ]);
+   ]);
 };
 
 
