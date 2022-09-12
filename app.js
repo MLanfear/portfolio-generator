dface7643043a77135ce1ebe5e2dc3a6a7526b96
MLanfear <109485865+MLanfear@users.noreply.github.com>
2022-09-12 04:04:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -83,6 +83,7 @@ const promptProject = portfolioData => {
       message: 'Would you like to enter another project?',
       default: false
     }
+   ]);
    .then(projectData => {
      portfolioData.projects.push(projectData);
      if (projectData.confirmAddProject) {
@@ -90,8 +91,7 @@ const promptProject = portfolioData => {
      } else {
        return portfolioData;
      }
-   };
- ]);  
+ )};  
 };
 
 
