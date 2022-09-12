fa54deaf5f17675b4648b0885776f84b26e680cd
MLanfear <109485865+MLanfear@users.noreply.github.com>
2022-09-12 01:21:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./sec/page-template');
+//const fs = require('fs');
 
-const pageHTML = generatePage(name, github);
+//const generatePage = require('./sec/page-template');
+
+//const pageHTML = generatePage(name, github);
 
 
 
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+//fs.writeFile('./index.html', pageHTML, err => {
+  //if (err) throw err;
   
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//  console.log('Portfolio complete! Check out index.html to see the output!');
+//});
