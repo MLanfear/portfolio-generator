dad2a4d44f65ee03c63a50e702368f48db57c0cf
MLanfear <maxjlanfear@gmail.com>
2022-09-12 06:55:06 -0700

    break time

--- sec/page-template.js
+++ sec/page-template.js
@@ -12,7 +12,16 @@ const generateAbout = aboutText => {
   `;
 };
 
-
+const generateProjects = projectsArr => {
+  return `
+    <section class="my-3" id="portfolio">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+      <div class="flex-row justify-space-between">
+        <!-- Leaving this empty as we'll dynamically insert project HTML here -->
+      </div>
+    </section>
+  `;
+};
 
 module.exports = templateData => {
   
@@ -46,6 +55,7 @@ module.exports = templateData => {
     </header>
     <main class="container my-5">
       ${generateAbout(about)}
+      ${generateProjects(projects)}
     </main>
     <footer class="container text-center py-3">
       <section class="my-3" id="about">
