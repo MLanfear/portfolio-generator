21d75a9d18f0dc43a873a4c21ec37db65d2f9baf
MLanfear <maxjlanfear@gmail.com>
2022-09-14 14:56:25 -0700

    fixed

--- app.js
+++ app.js
@@ -157,15 +157,25 @@ const promptProject = portfolioData => {
 
   
 promptUser()
-    .then(promptProject)
-    .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
 
-        fs.writeFile('./index.html', pageHTML, err => {
-          if (err) throw err;
-  
-      //  console.log('Portfolio complete! Check out index.html to see the output!');
+    fs.writeFile('./dist/index.html', pageHTML, err => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('Page created! Check out index.html in this directory to see it!');
+
+      fs.copyFile('./sec/style.css', './dist/style.css', err => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('Style sheet copied successfully!');
       });
+    });
   });
   
 
